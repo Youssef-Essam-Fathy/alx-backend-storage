c9853a6f8330aff2cf1accfaaf6ccb0cdea96053
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-06-13 14:00:16 +0300

    unique users

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,5 @@
--- Creates a table with unique users.
-DROP TABLE IF EXISTS users;
-CREATE TABLE users (
+-- SQL script that creates a table with unique users.
+CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)
