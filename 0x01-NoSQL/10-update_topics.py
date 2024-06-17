eb378b79a2d950bad3a86b1650c7556a84c7fd07
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-06-17 16:08:59 +0300

    update topics of a school document in MongoDB collection

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -7,12 +7,19 @@ def update_topics(mongo_collection, name, topics):
     Change all topics of a school document based on the name.
 
     Args:
-        mongo_collection (pymongo.collection.Collection): The MongoDB collection to query.
+        mongo_collection (pymongo.collection.Collection):
+        The MongoDB collection to query.
         name (str): The school name to update.
-        topics (list of str): The list of topics approached in the school.
+        topics (list of str): The list of topics
+        approached in the school.
+
+    Returns:
+        pymongo.results.UpdateResult:
+        The result of the update operation.
+
     """
-    mongo_collection.update_one(
+    res = mongo_collection.update_many(
         {'name': name},
         {'$set': {'topics': topics}}
-    )
-
+        )
+    return res
