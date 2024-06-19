7a0cf9906258df2f6da456ed18fafb71c72d9350
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-06-20 00:35:01 +0300

    adding web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,38 +1,40 @@
 #!/usr/bin/env python3
-'''A module with tools for request caching and tracking.
-'''
+"""
+A module with tools for request caching and tracking.
+"""
+
 import redis
 import requests
 from functools import wraps
 from typing import Callable
 
-
+# Create a Redis instance
 redis_store = redis.Redis()
-'''The module-level Redis instance.
-'''
-
 
 def data_cacher(method: Callable) -> Callable:
-    '''Caches the output of fetched data.
-    '''
+    """
+    Caches the output of fetched data and tracks the number of accesses.
+    """
     @wraps(method)
-    def invoker(url) -> str:
-        '''The wrapper function for caching the output.
-        '''
+    def invoker(url: str) -> str:
+        """
+        Wrapper function for caching the output.
+        """
         redis_store.incr(f'count:{url}')
         result = redis_store.get(f'result:{url}')
         if result:
             return result.decode('utf-8')
         result = method(url)
-        redis_store.set(f'count:{url}', 0)
         redis_store.setex(f'result:{url}', 10, result)
         return result
     return invoker
 
-
 @data_cacher
 def get_page(url: str) -> str:
-    '''Returns the content of a URL after caching the request's response,
+    """
+    Returns the content of a URL after caching the request's response,
     and tracking the request.
-    '''
-    return requests.get(url).text
+    """
+    response = requests.get(url)
+    response.raise_for_status()
+    return response.text
